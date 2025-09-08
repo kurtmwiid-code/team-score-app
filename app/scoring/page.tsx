a4bc3365df1ffd7c1916d05e57bda2827181e821
kurtmwiid-code <kurtmwiid@gmail.com>
2025-09-08 21:27:13 +0200

    tried new fix for scoring not submitting

--- app/scoring/page.tsx
+++ app/scoring/page.tsx
@@ -32,10 +32,7 @@ type Submission = {
 const supabaseUrl = "https://qcfgxqtlkqttqbrwygol.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjZmd4cXRsa3F0dHEtnd5Z29sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2MzczNjcsImV4cCI6MjA3MjIxMzM2N30.rN-zOVDOtJdwoRSO0Yi5tr3tK3MGVPJhwvV9yBjUnF0";
 
-let supabase: SupabaseClient | null = null;
-if (supabaseUrl && supabaseKey) {
-  supabase = createClient(supabaseUrl, supabaseKey);
-}
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // --- Data Arrays ---
 const salesReps = [
